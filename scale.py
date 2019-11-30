b65691b01ede3e27c909535a3d5d9bcc0ece847a
LaboiteNoire <laboitenoirelbnlbn@gmail.com>
2019-11-30 18:41:25 +0100

    il faut bien un deuxieme fichier

---
+++ scale.py
@@ -0,0 +1,28 @@
+import pygame
+Reso = (600,600)
+
+pygame.init()
+launche = True
+
+pygame.display.set_caption("SgCsF")
+Wind = pygame.display.set_mode(Reso)
+
+EntityImg = pygame.image.load("Entity.png")
+SuGr = (64,64,64)
+
+Wind.fill(SuGr)
+EntityImg = pygame.transform.scale(EntityImg,(300,300))
+
+R = EntityImg.get_rect()
+rect = R.move((0,0))
+
+while launche:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            launche = False
+            
+    Wind.fill(SuGr)
+    x = 300
+
+    Wind.blit(EntityImg,rect)
+    pygame.display.flip()
