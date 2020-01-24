6d275c81d3a39e7661ad3a4e1ca8c0bdd60b701a
LaBoiteNoire <49619049+LaboiteNoire@users.noreply.github.com>
2020-01-24 17:05:39 +0100

    Main
    je sais plus quelle version

--- Main.py
+++ Main.py
@@ -45,7 +45,7 @@ def Fpx1211importGeneral():
             print("Impossible de charger le module pygame")
             metaD = "fail"
     try:          
-        import numpy
+        import numpy as np
         metaD = "succes"
     except ImportError:
             print("Impossible de charger le module numpy")
@@ -57,6 +57,46 @@ def Fpx1211importGeneral():
             print("Impossible de charger le module simpleaudio")
             metaD = "fail"
     return metaD
+"""
+24/01/2020
+premiere version de Saundoefekuto litteralement bruitage.
+deve : Mahli,Cloe
+concu initialement pour l'architecture general c'est fonction est une fonction
+de gestion des ressources fichiers audio, qui ne prend que tres peux sur le cpu et le
+gpu.
+Elle a pour but d'eviter et de cibler les problemes qui pourraient-etre liés a
+tel ou tel selection de son puis de le jouer en stereo avec le volume donné.
+NB:ne prend en compte que les fichier .wav.
+Il est recommander de n'utiliser cette fonction que pour les objets d'environnements.
+Cette fonction ne fonctionne que si le mixer a été initialiser avec 2 channels.
+"""
+def Fpx2401Saundoefekuto(name,,vX,vY):
+    soundFx = pygame.mixer.Sound(str(name)+'.wav')
+    channelFx = soundFx.play()
+    if vX > 1:
+        vX = 1
+    else:
+        vX = vX
+    if vY > 1:
+        vY = 1
+    else:
+        vY = vY
+    try:
+        channel.set_volume(vX, vY)
+        MetaD = ["succes"]
+    except AttributeError:
+        MetaD = ["succes","attribut error"]
+"""
+17/01/2020
+premiere version de variablesStatic
+deve : Mahli
+concu initialement pour l'architecture general c'est fonction est une fonction
+de gestion des ressources statique , qui ne prend que tres peux sur le cpu et le
+gpu.
+Elle a pour but d'englober des variables qui serront utiliser par tous les deves.
+"""
+def Fpx1701variablesStatics():
+    pi = np.pi
 """
 12/11/2019
 premiere version de loadImages
@@ -104,7 +144,7 @@ def Fpx0401loadImages(acces,scale):
             metaD = ["succes",str(scale)]
     except pygame.error:
         img = "error"
-        metaD = "fail"
+        metaD = ["fail"]
         print("impossible de charger l'image png",str(acces))
     return img , metaD
 """
@@ -118,7 +158,7 @@ Elle a pour but d'avoir une meilleur gestion de la creation de fenetres.
 notes:dans de futures versions il serra possible d'y isserrer des images dans le titres.
 """
 def Gpx1211createWindow(typeW,H,L,titl):
-    if typeW == "F":
+    if typeW == "F"or "f":
         Wind =pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         metaD = [("fullscreen" , "title="+str(titl))]
     else:
@@ -126,5 +166,14 @@ def Gpx1211createWindow(typeW,H,L,titl):
         metaD = [(str(L) , "x" , str(H) , "title="+str(titl))]
     pygame.dysplay.set_caption(str(titl))
     return Wind,metaD
-
+"""
+Saundoefekuto
+"""
+class Phiggs:
+    def __init__(self):
+        return 0
+    def pi():
+        return 3.14
+    pi = staticmethod(pi)
+    
     
